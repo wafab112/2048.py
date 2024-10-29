dc6dc01c42cf5912926d5883b59e0a1564829c39
wafab112 <wafab112@gmail.com>
2024-10-29 15:34:49 +0100

    add corresponding test

--- test.py
+++ test.py
@@ -83,5 +83,13 @@ class TestGridMerge(unittest.TestCase):
         grid.move(Direction.UP)
         self.assertEqual(ex, grid)
 
+    def test_2_2_4_0(self):
+        ex = Grid.WithPos((0, 0, 4), (1, 0, 4))
+
+        grid = Grid.WithPos((0, 0, 2), (1, 0, 2), (2, 0, 4))
+        grid.move(Direction.UP)
+        self.assertEqual(ex, grid)
+
+
 if __name__ == "__main__":
     unittest.main()
